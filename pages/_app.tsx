f1d6b6a4d5a5fd2a8663efb810abdc953eeb1dfa
m-rejdych <rejdych.m.r@gmail.com>
2022-08-06 15:49:14 +0200

    Initial trpc setup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { withTRPC } from '@trpc/next';
+
+import type { AppRouter } from './api/trpc/[trpc]';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default withTRPC<AppRouter>({
+  config: () => {
+    return {
+      url: `${process.env.URL as string}/api/trpc`,
+    };
+  },
+  ssr: true,
+})(MyApp);
