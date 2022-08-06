c2de1504ef66b33022059b5f07ea4064448f0214
m-rejdych <rejdych.m.r@gmail.com>
2022-08-06 17:18:09 +0200

    Added register mutation.

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -2,12 +2,17 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 
-const appRouter = trpc.router().query('hello', {
-  input: z.string(),
-  resolve: ({ input }) => {
-    return `Hi ${input}`;
-  },
-});
+import authRouter from '../../../server/routers/auth';
+
+const appRouter = trpc
+  .router()
+  .query('hello', {
+    input: z.string(),
+    resolve: ({ input }) => {
+      return `Hi ${input}`;
+    },
+  })
+  .merge(authRouter);
 
 export type AppRouter = typeof appRouter;
 
---
+++ server/routers/auth.ts
@@ -0,0 +1,27 @@
+import * as trpc from '@trpc/server';
+import { z } from 'zod';
+import { hash } from 'bcryptjs';
+
+import { prisma } from '../../server/prisma';
+
+const authRouter = trpc.router().mutation('register', {
+  input: z.object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/),
+    username: z.string(),
+  }),
+  resolve: async ({ input: { password, ...rest } }) => {
+    const hashedPassword = await hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: { ...rest, password: hashedPassword },
+      select: { email: true },
+    });
+
+    return { email: user.email, password };
+  },
+});
+
+export default authRouter;
