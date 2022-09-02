a785452239f9dab0d435751c79d9ed71eb456b20
m-rejdych <rejdych.m.r@gmail.com>
2022-09-02 23:08:57 +0200

    Implemented correct answers count validation.

--- src/server/routers/quiz.ts
+++ src/server/routers/quiz.ts
@@ -40,19 +40,41 @@ const quizRouter = createRouter()
       ctx: { prisma, userId },
       input: { title, questions },
     }) => {
-      if (questions?.some(isDuplicated('title'))) {
-        throw new trpc.TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'Duplicated question titles.',
-        });
-      }
+      if (questions) {
+        if (questions.some(isDuplicated('title'))) {
+          throw new trpc.TRPCError({
+            code: 'BAD_REQUEST',
+            message: 'Duplicated question titles.',
+          });
+        }
 
-      if (
-        questions?.some(({ answers }) => answers?.some(isDuplicated('content')))
-      ) {
-        throw new trpc.TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'Duplicated answer contents.',
+        if (
+          questions.some(({ answers }) =>
+            answers?.some(isDuplicated('content')),
+          )
+        ) {
+          throw new trpc.TRPCError({
+            code: 'BAD_REQUEST',
+            message: 'Duplicated answer contents.',
+          });
+        }
+
+        questions.forEach(({ answers }) => {
+          if (!answers?.length) return;
+
+          let correctCount = 0;
+
+          for (const { isCorrect } of answers) {
+            if (isCorrect) correctCount++;
+            if (correctCount > 1) break;
+          }
+
+          if (correctCount !== 1) {
+            throw new trpc.TRPCError({
+              code: 'BAD_REQUEST',
+              message: 'One answer has to be marked as correct.',
+            });
+          }
         });
       }
 
